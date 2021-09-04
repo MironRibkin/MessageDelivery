d717796eb0c0ebb992d501c0a7735742331b9751
Miron <miron.ribkin98@mail.ru>
2021-09-04 23:30:42 +0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,5 @@
+const button = document.querySelectorAll(".submt")
+const input = document.querySelectorAll(".mesage")
+button.addEventListener("click", function () {
+ var mess = document.getElementById('mes')
+})

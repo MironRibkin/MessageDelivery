31b713ace3fc4dd71bb0c2b8036c593b4207accb
Miron <miron.ribkin98@mail.ru>
2021-09-05 21:56:30 +0300

    добавил логику на кнопку, сделал всплывающее окно.

--- main.js
+++ main.js
@@ -1,8 +1,27 @@
-const button = document.querySelectorAll(".submt")
-const input = document.querySelectorAll(".mesage")
-button.addEventListener("click", function () {
- var mess = document.getElementById('mes')
+const addbutton = document.querySelector(".addButton")
+const input = document.querySelector(".mesage")
+const content = document.querySelector(".content")
+const noMessWindow = document.querySelector(".no-mess-window")
+
+function validateMessageInput() {
+    if (input.value === "") {
+        noMessWindow.style.display = "block";
+        setTimeout(function () {
+            noMessWindow.style.display = "none"
+        }, 2000)
+    }
+}
+
+function createMessage() {
+    const message = document.createElement("div");
+    message.className = "texmes";
+    message.innerHTML = input.value;
+    return message;
+}
+
+addbutton.addEventListener("click", function () {
+    validateMessageInput();
+    content.appendChild(createMessage());
+    input.value = "";
 })
-
-
 
